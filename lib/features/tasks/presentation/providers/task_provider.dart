e731ee3d5cde54ed712448dc647a5140e0c94b3f
Afzal Momin <afzalmomin2003@gmail.com>
2025-08-09 01:04:48 +0530

    added list for notifier

--- lib/features/tasks/presentation/providers/task_provider.dart
+++ lib/features/tasks/presentation/providers/task_provider.dart
@@ -3,4 +3,4 @@ import 'package:railse_assignment/features/tasks/domain/entities/task_entity.dar
 import 'package:railse_assignment/features/tasks/presentation/notifier/task_notifier.dart';
 
 
-final taskNotifier = StateNotifierProvider<TaskManagerNotifier,TaskEntity>((ref){return TaskManagerNotifier();});
+final taskNotifier = StateNotifierProvider<TaskManagerNotifier,List<TaskEntity>>((ref){return TaskManagerNotifier();});
