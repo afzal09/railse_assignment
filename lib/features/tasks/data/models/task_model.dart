980e17ae9c909c926f7293ed450b30778ace5592
Afzal Momin <afzalmomin2003@gmail.com>
2025-08-08 20:57:47 +0530

    added id field for task identification

--- lib/features/tasks/data/models/task_model.dart
+++ lib/features/tasks/data/models/task_model.dart
@@ -2,6 +2,7 @@ import 'package:railse_assignment/features/tasks/domain/entities/task_entity.dar
 
 class TaskModel extends TaskEntity{
   TaskModel({
+    required super.id, 
     required super.user,
     required super.title,
     required super.description,
@@ -23,6 +24,7 @@ class TaskModel extends TaskEntity{
   }
     factory TaskModel.fromEntity(TaskEntity entity) {
     return TaskModel(
+      id: '',
       user: entity.user,
       title: entity.title,
       description: entity.description,
@@ -37,6 +39,7 @@ class TaskModel extends TaskEntity{
   /// Convert model back to entity
   TaskEntity toEntity() {
     return TaskEntity(
+      id: '',
       user: user,
       title: title,
       description: description,
@@ -48,6 +51,7 @@ class TaskModel extends TaskEntity{
     );
   }
   TaskModel updateModel({
+    String? id,
     String? title,
     String? description,
     DateTime? startedDate,
@@ -57,6 +61,7 @@ class TaskModel extends TaskEntity{
     bool? isCompleted    
   }){
     return TaskModel(
+      id: id ?? this.id,
       user: user,
       title: title ?? this.title,
       description: description ?? this.description,
@@ -64,7 +69,7 @@ class TaskModel extends TaskEntity{
       dueDate: dueDate ?? this.dueDate,
       priority: priority ?? this.priority,
       status: status ?? this.status,
-      isCompleted: isCompleted ?? this.isCompleted
+      isCompleted: isCompleted ?? this.isCompleted,
     );
   }
 }
