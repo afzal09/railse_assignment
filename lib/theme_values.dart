9b120705623de514bffe402766afca85ad9ab706
Afzal Momin <afzalmomin2003@gmail.com>
2025-08-08 20:58:26 +0530

    added more color values

--- lib/theme_values.dart
+++ lib/theme_values.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
 
-// Main background color for the application frame
-const Color lightGrayishBlue = Color(0xFFF0F3FF);
+class AppColors {
+  AppColors._();
+  // Main background color for the application frame
+  static const Color lightGrayishBlue = Color(0xFFF0F3FF);
+  static const Color blackColor = Color(0xFF212121);
 
-// Background color for individual task cards
-const Color white = Color(0xFFFFFFFF);
+  static const Color greyColor = Color(0xFF9E9E9E);
+  static const Color lightGreyColor = Color(0xFFF5F5F5);
 
-// Dashed border color and some secondary text
-const Color lightGrayBorder = Color(0xFFD9D9D9);
+  // Background color for individual task cards
+  static const Color white = Color(0xFFFFFFFF);
 
-// Accent bar color for overdue tasks
-const Color overdueBar = Color(0xFF5C6BC0);
+  // Dashed border color and some secondary text
+  static const Color lightGrayBorder = Color(0xFFD9D9D9);
 
-// Main text color for titles and names
-const Color darkGray = Color(0xFF212121);
+  // Accent bar color for overdue tasks
+  static const Color overdueBar = Color(0xFF5C6BC0);
 
-// Color for 'Overdue' status and 'High Priority' text
-const Color overDueStatus = Color(0xFFE53935);
+  // Main text color for titles and names
+  static const Color darkGray = Color(0xFF212121);
 
-// Color for 'Completed' status and checkmarks
-const Color completedStatus = Color(0xFF4CAF50);
+  // Color for 'Overdue' status and 'High Priority' text
+  static const Color overDueStatus = Color(0xFFE53935);
 
-// Color for 'Due' status
-const Color dueStatus = Color(0xFFFFB300);
+  // Color for 'Completed' status and checkmarks
+  static const Color completedStatus = Color(0xFF4CAF50);
 
-// Color for 'Start Task' buttons and icons
-const Color startStatus = Color(0xFF7E57C2);
+  // Color for 'Due' status
+  static const Color dueStatus = Color(0xFFFFB300);
+
+  // Color for 'Start Task' buttons and icons
+  static const Color startStatus = Color(0xFF7E57C2);
+  // Priority Colors (can be distinct or derived from the main palette)
+  static const Color veryHighPriority = Color(0xFFFF0400);
+  static const Color highPriorityColor = Color(0xFFE53935); // Strong Red
+  static const Color mediumPriorityColor = Color(0xFFFFB300); // Vivid Orange
+  static const Color lowPriorityColor = Color(0xFF43A047); // Bright Green
+}
